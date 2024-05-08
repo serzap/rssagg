807ab9d6459a80766adf4f5b2cfcca97805a6dc1
Serhii Zaporozhchenko <serhii.zaporozhchenko@gmail.com>
2024-05-08 11:59:16 +0100

    Fetch feeds

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -31,10 +32,12 @@ func main() {
 	if err != nil {
 		log.Fatal("Can't connect to database:", err)
 	}
-
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
